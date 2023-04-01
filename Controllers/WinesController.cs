1be30c2de96f4318254bb21ccb6600191d688c6d
Andres Pineda <pineda.andres@gmail.com>
2023-04-01 15:19:17 -0400

    add wine controller

---
+++ Controllers/WinesController.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Api.Models;
+
+namespace Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class WinesController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public WinesController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Wines
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Wine>>> GetWines()
+        {
+          if (_context.Wines == null)
+          {
+              return NotFound();
+          }
+            return await _context.Wines.ToListAsync();
+        }
+
+        // GET: api/Wines/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Wine>> GetWine(long id)
+        {
+          if (_context.Wines == null)
+          {
+              return NotFound();
+          }
+            var wine = await _context.Wines.FindAsync(id);
+
+            if (wine == null)
+            {
+                return NotFound();
+            }
+
+            return wine;
+        }
+
+        // PUT: api/Wines/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutWine(long id, Wine wine)
+        {
+            if (id != wine.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(wine).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!WineExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Wines
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Wine>> PostWine(Wine wine)
+        {
+          if (_context.Wines == null)
+          {
+              return Problem("Entity set 'AppDbContext.Wines'  is null.");
+          }
+            _context.Wines.Add(wine);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetWine", new { id = wine.Id }, wine);
+        }
+
+        // DELETE: api/Wines/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteWine(long id)
+        {
+            if (_context.Wines == null)
+            {
+                return NotFound();
+            }
+            var wine = await _context.Wines.FindAsync(id);
+            if (wine == null)
+            {
+                return NotFound();
+            }
+
+            _context.Wines.Remove(wine);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool WineExists(long id)
+        {
+            return (_context.Wines?.Any(e => e.Id == id)).GetValueOrDefault();
+        }
+    }
+}
